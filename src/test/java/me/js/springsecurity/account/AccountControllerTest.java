34aae19650b5af0ccaf9487b2af569ae3bfdb4d5
JS <shk3029>
2020-02-22 21:43:44 +0900

    스프링 시큐리티 테스트 2

--- src/test/java/me/js/springsecurity/account/AccountControllerTest.java
+++ src/test/java/me/js/springsecurity/account/AccountControllerTest.java
@@ -9,8 +9,12 @@ import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,6 +26,9 @@ public class AccountControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
 
     @Test
     public void index_anonymous() throws Exception {
@@ -75,5 +82,28 @@ public class AccountControllerTest {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    @Transactional // 다른 테스트에 영향을 주지 않도록 테스트가 끝나면 롤백을 시켜줌
+    public void login() throws Exception {
+        Account account = new Account();
+        account.setUserName("js");
+        account.setPassword("123");
+        account.setRole("USER");
+        accountService.createNew(account);
+        mockMvc.perform(formLogin().user("js").password("123"))
+                .andExpect(authenticated());
+    }
+
+    @Test
+    @Transactional
+    public void login_fail() throws Exception {
+        Account account = new Account();
+        account.setUserName("js");
+        account.setPassword("1234");
+        account.setRole("USER");
+        accountService.createNew(account);
+        mockMvc.perform(formLogin().user("jss").password("123"))
+                .andExpect(unauthenticated());
+    }
 
 }
